df2b545248de3035aee4e8a66cfecd3587587604
Mona <monaakbarpour1@gmail.com>
2021-09-07 00:28:48 +0200

    building better calculator

--- CalculatorCSharp/CalculatorCSharp/Program.cs
+++ CalculatorCSharp/CalculatorCSharp/Program.cs
@@ -6,12 +6,39 @@ namespace CalculatorCSharp
     {
         static void Main(string[] args)
         {
+            //Console.Write("Enter a number:");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter another number:");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1 + num2);
+
+            //building a better calculator
             Console.Write("Enter a number:");
             double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter another number:");
+
+            Console.Write("Enter Operator:");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number:");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(num1 + num2);
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            }else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            }else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+            }else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+            }else
+            {
+                Console.WriteLine("invalid operator");
+            }
 
             Console.ReadLine();
         }
